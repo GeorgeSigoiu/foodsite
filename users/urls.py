4dc8264815d78d901ca18ce25217b884eed6d812
George Sigoiu <george_sigoiu@yahoo.com>
2021-11-29 17:15:08 +0200

    url path modified

--- users/urls.py
+++ users/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.profiles, name="profiles"),
+    path('create-user', views.createUser, name="create-user"),
+    path('login-user', views.loginUser, name="login-user"),
+    path('logout-user', views.logoutUser, name="logout-user"),
+    path('user-profile/<str:username>', views.showProfile, name="user-profile"),
 ]
