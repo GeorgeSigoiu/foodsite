75a9ce906472f5f3964307dc9cda6daa45c57d35
George Sigoiu <george_sigoiu@yahoo.com>
2021-11-30 18:09:38 +0200

    url path personal info

--- users/urls.py
+++ users/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('login-user', views.loginUser, name="login-user"),
     path('logout-user', views.logoutUser, name="logout-user"),
     path('user-profile/<str:username>', views.showProfile, name="user-profile"),
-    path('update-user/',
-         views.updatePersonalInfo, name="update-user"),
+    path('update-user/', views.updatePersonalInfo, name="update-user"),
+    path('create-bill/<str:bill_number>', views.createBill, name="create-bill"),
 ]
