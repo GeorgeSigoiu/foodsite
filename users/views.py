b9a1527fbfdadfc9f6d26f88e61d053f94a552d0
George Sigoiu <george_sigoiu@yahoo.com>
2021-11-30 14:35:45 +0200

    update for login bugs

--- users/views.py
+++ users/views.py
@@ -16,6 +16,11 @@ def createUser(request):
     username = trim(result["username"])
     email = trim(result["email"])
     password = trim(result["password"])
+    existsUsername = len(Profile.objects.filter(username=username)) > 0
+    if existsUsername:
+        return render(request, "login.html", {"message": "Exista deja acest utilizator!"})
+    if len(username) == 0 or len(password) == 0 or len(email) == 0:
+        return render(request, "login.html", {"message": "Nu se poate crea contul: toate campurile trebuie completate!"})
     profile = Profile.create(email=email, username=username, password=password)
     profile.save()
     login(profile)
@@ -26,20 +31,48 @@ def loginUser(request):
     result = dict(request.POST)
     username = trim(result["username"])
     password = trim(result["password"])
+    if len(username) == 0 or len(password) == 0:
+        return render(request, "login.html", {"message": "Nu au fost completate toate campurile!"})
     profileExists = False
-    profile = Profile.objects.filter(
-        username=username).filter(password=password)[0]
+    profileUsername = Profile.objects.filter(username=username)
+    profile = None
+    if profileUsername:
+        profilePassword = Profile.objects.filter(
+            username=username).filter(password=password)
+        if profilePassword:
+            profile = profilePassword[0]
     if profile:
         profileExists = True
     if profileExists:
         login(profile)
         return redirect("/")
     else:
-        return render(request, "login.html", {"message": True})
+        return render(request, "login.html", {"message": "Numele de utilizator sau parola nu sunt corecte!"})
 
 
 def showProfile(request, username):
     global profile_logged_in
+    context = {
+        "profile": profile_logged_in
+    }
+    return render(request, 'users/profiles.html', context)
+
+
+def updatePersonalInfo(request):
+    global profile_logged_in
+    result = dict(request.POST)
+    print(result)
+    firstname = trim(result["firstname"])
+    surname = trim(result["surname"])
+    phone = trim(result["phone"])
+    email = trim(result["email"])
+    address = trim(result["address"])
+    profile_logged_in.firstname = firstname
+    profile_logged_in.surname = surname
+    profile_logged_in.phone = phone
+    profile_logged_in.email = email
+    profile_logged_in.address = address
+    profile_logged_in.save()
     context = {
         "profile": profile_logged_in
     }
@@ -56,7 +89,6 @@ def logoutUser(request):
 def login(user):
     global profile_logged_in
     profile_logged_in = user
-    print(type(user))
 
 
 def logout():
