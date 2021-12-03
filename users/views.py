25f462443f69e3cb60dd77f7d5ae78dbef2054e1
George Sigoiu <george_sigoiu@yahoo.com>
2021-12-03 10:17:10 +0200

    refactoring methods

--- users/views.py
+++ users/views.py
@@ -5,7 +5,6 @@ import products.views
 
 from users.models import Bill, Profile
 
-# Create your views here.
 profile_logged_in = None
 
 
@@ -22,8 +21,15 @@ def createUser(request):
     if existsUsername:
         return render(request, "login.html", {"message": "Exista deja acest utilizator!"})
     if len(username) == 0 or len(password) == 0 or len(email) == 0:
-        return render(request, "login.html", {"message": "Nu se poate crea contul: toate campurile trebuie completate!"})
-    profile = Profile.create(email=email, username=username, password=password)
+        return render(request, "login.html",
+                      {"message": "Nu se poate crea contul: toate campurile trebuie completate!"})
+
+    profile = Profile.Builder() \
+        .set_username(username) \
+        .set_password(password) \
+        .set_email(email) \
+        .build()
+
     profile.save()
     login(profile)
     return redirect("/")
@@ -39,8 +45,9 @@ def loginUser(request):
     profileUsername = Profile.objects.filter(username=username)
     profile = None
     if profileUsername:
-        profilePassword = Profile.objects.filter(
-            username=username).filter(password=password)
+        profilePassword = Profile.objects \
+            .filter(username=username) \
+            .filter(password=password)
         if profilePassword:
             profile = profilePassword[0]
     if profile:
@@ -64,23 +71,32 @@ def showProfile(request, username):
 def updatePersonalInfo(request):
     global profile_logged_in
     result = dict(request.POST)
-    print(result)
-    firstname = trim(result["firstname"])
-    surname = trim(result["surname"])
-    phone = trim(result["phone"])
-    email = trim(result["email"])
-    address = trim(result["address"])
+    address, email, firstname, phone, surname = get_information_personal_data(result)
+    set_information_personal_data(address, email, firstname, phone, surname)
+    context = {
+        "profile": profile_logged_in,
+        "numberOfProducts": getNumberOfProducts(),
+    }
+    return render(request, 'users/profiles.html', context)
+
+
+def set_information_personal_data(address, email, firstname, phone, surname):
+    global profile_logged_in
     profile_logged_in.firstname = firstname
     profile_logged_in.surname = surname
     profile_logged_in.phone = phone
     profile_logged_in.email = email
     profile_logged_in.address = address
     profile_logged_in.save()
-    context = {
-        "profile": profile_logged_in,
-        "numberOfProducts": getNumberOfProducts(),
-    }
-    return render(request, 'users/profiles.html', context)
+
+
+def get_information_personal_data(result):
+    firstname = trim(result["firstname"])
+    surname = trim(result["surname"])
+    phone = trim(result["phone"])
+    email = trim(result["email"])
+    address = trim(result["address"])
+    return address, email, firstname, phone, surname
 
 
 def logoutUser(request):
@@ -118,17 +134,16 @@ def trim(string):
 
 def createBill(request, bill_number):
     bill = Bill.objects.get(bill_number=bill_number)
-    name = bill.name
-    address = bill.address
-    phone = bill.phone
-    email = bill.email
-    timestamp = str(bill.created_at)[:10]
-    totalPrice = bill.price
-    deliveryCost = 10
-    if totalPrice > 60:
-        deliveryCost = 0
-    productsString = bill.products
+    address, deliveryCost, email, name, phone, productsString, timestamp, totalPrice = get_bill_data(bill)
     productsToShow = render_products_from_string(productsString)
+    context = get_bill_context(address, deliveryCost, email, name, phone, productsToShow, timestamp, totalPrice)
+    pdf = products.views.render_to_pdf('bill.html', context)
+    response = HttpResponse(pdf, content_type='application/pdf')
+    response['Content-Disposition'] = f'attachment; filename="{bill}.pdf"'
+    return response
+
+
+def get_bill_context(address, deliveryCost, email, name, phone, productsToShow, timestamp, totalPrice):
     context = {
         'pagesize': 'A4',
         'name': name,
@@ -140,10 +155,21 @@ def createBill(request, bill_number):
         "deliveryCost": deliveryCost,
         'timestamp': timestamp,
     }
-    pdf = products.views.render_to_pdf('bill.html', context)
-    response = HttpResponse(pdf, content_type='application/pdf')
-    response['Content-Disposition'] = f'attachment; filename="{bill}.pdf"'
-    return response
+    return context
+
+
+def get_bill_data(bill):
+    name = bill.name
+    address = bill.address
+    phone = bill.phone
+    email = bill.email
+    timestamp = str(bill.created_at)[:10]
+    totalPrice = bill.price
+    deliveryCost = 10
+    if totalPrice > 60:
+        deliveryCost = 0
+    productsString = bill.products
+    return address, deliveryCost, email, name, phone, productsString, timestamp, totalPrice
 
 
 def render_products_from_string(productsString):
@@ -156,11 +182,10 @@ def render_products_from_string(productsString):
         title = productParts[0].replace("-", " ")
         quantity = int(productParts[1])
         priceTotal = int(productParts[2])
-        priceUnit = priceTotal/quantity
+        priceUnit = priceTotal / quantity
         newProduct = MyProduct(title, priceUnit)
         element = (newProduct, quantity, priceTotal)
         productsList.append(element)
-
     return productsList
 
 
