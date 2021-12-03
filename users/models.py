cb13954e870e0ecf5e658be7b0db1dbe59eca592
George Sigoiu <george_sigoiu@yahoo.com>
2021-12-03 10:15:48 +0200

    added builder pattern

--- users/models.py
+++ users/models.py
@@ -1,9 +1,97 @@
 import uuid
 from django.db import models
+
+
 # Create your models here.
 
 
+class Bill(models.Model):
+    class Builder:
+        def set_address(self, address):
+            self.address = address
+            return self
+
+        def set_name(self, name):
+            self.name = name
+            return self
+
+        def set_phone(self, phone):
+            self.phone = phone
+            return self
+
+        def set_email(self, email):
+            self.email = email
+            return self
+
+        def set_products(self, products):
+            self.products = products
+            return self
+
+        def set_price(self, price):
+            self.price = price
+            return self
+
+        def build(self):
+            return Bill.createInstance(self)
+
+    bill_number = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    name = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+    phone = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    products = models.TextField(null=True, blank=True)
+    price = models.IntegerField(default=0, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+
+    @classmethod
+    def createInstance(cls, builder):
+        bill = cls(name=builder.name,
+                   address=builder.address,
+                   phone=builder.phone,
+                   email=builder.email,
+                   products=builder.products,
+                   price=builder.price)
+        return bill
+
+    def __str__(self):
+        return str(self.created_at)[:19] + " - " + str(self.bill_number)[:6]
+
+
 class Profile(models.Model):
+    class Builder:
+        firstname = surname = email = phone = address = ""
+
+        def set_firstname(self, firstname):
+            self.firstname = firstname
+            return self
+
+        def set_surname(self, surname):
+            self.surname = surname
+            return self
+
+        def set_email(self, email):
+            self.email = email
+            return self
+
+        def set_username(self, username):
+            self.username = username
+            return self
+
+        def set_password(self, password):
+            self.password = password
+            return self
+
+        def set_phone(self, phone):
+            self.phone = phone
+            return self
+
+        def set_address(self, address):
+            self.address = address
+            return self
+
+        def build(self):
+            return Profile.createInstance(self)
+
     firstname = models.CharField(max_length=200, blank=True, null=True)
     surname = models.CharField(max_length=200, blank=True, null=True)
     email = models.EmailField(max_length=200, blank=True, null=True)
@@ -16,31 +104,15 @@ class Profile(models.Model):
                           primary_key=True, editable=False)
 
     @classmethod
-    def create(cls, email, username, password):
-        profile = cls(email=email, username=username, password=password,
-                      firstname="", surname="", phone="", address="")
+    def createInstance(cls, builder):
+        profile = cls(username=str(builder.username),
+                      surname=str(builder.surname),
+                      firstname=str(builder.firstname),
+                      password=str(builder.password),
+                      phone=str(builder.phone),
+                      address=str(builder.address),
+                      email=str(builder.email))
         return profile
 
     def __str__(self):
         return self.username
-
-
-class Bill(models.Model):
-    bill_number = models.UUIDField(default=uuid.uuid4, unique=True,
-                                   primary_key=True, editable=False)
-    name = models.CharField(max_length=200)
-    address = models.CharField(max_length=200)
-    phone = models.CharField(max_length=200)
-    email = models.CharField(max_length=200)
-    products = models.TextField(null=True, blank=True)
-    price = models.IntegerField(default=0, null=True, blank=True)
-    created_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
-
-    @classmethod
-    def create(cls, name, address, phone, email, products, price):
-        bill = cls(name=name, address=address, phone=phone,
-                   email=email, products=products, price=price)
-        return bill
-
-    def __str__(self):
-        return str(self.created_at)[:19]+" - "+str(self.bill_number)[:6]
